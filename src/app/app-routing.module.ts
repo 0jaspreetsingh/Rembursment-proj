ca32869d8cd48aaeba3a2b57358ae6bfcb5d232b
0jaspreetsingh <49236940+0jaspreetsingh@users.noreply.github.com>
2019-11-04 00:06:59 +0530

    working on signup ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signUp', component: SignUpComponent }
 ];
 
 @NgModule({
